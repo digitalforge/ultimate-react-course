78e6110e82a05a0fe5abecf9ae933f83d08c3746
digitalforge <digitalforge@outlook.com>
2026-01-31 12:47:33 -0500

    Atomic blog site added

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Product from './pages/Product/Product'
 import Homepage from './pages/Homepage/Homepage'
 import Pricing from './pages/Product/Pricing'
@@ -43,10 +43,7 @@ function App() {
           <Route path='pricing' element={<Pricing />} />
           <Route path='login' element={<Login />} />
           <Route path='app' element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate to='cities' replace />} />
             <Route
               path='cities'
               element={<CityList cities={cities} isLoading={isLoading} />}
