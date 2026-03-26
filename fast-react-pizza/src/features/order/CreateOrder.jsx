e61808af0c7fb1ddd3e1e50ba5fd097a9b43a871
digitalforge <digitalforge@outlook.com>
2026-03-26 19:20:29 -0400

    implemented thunk for getting geolocation in the redux userSlice store

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
 import Button from '../../ui/Button'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { getCart, clearCart, getTotalPrice } from '../cart/cartSlice'
 import EmptyCart from '../cart/EmptyCart'
 import store from '../../store'
 import { formatCurrency } from '../../utils/helpers'
+import { fetchAddress } from '../user/userSlice'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -52,12 +53,16 @@ function CreateOrder() {
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0
   const totalPrice = totalCartPrice + priorityPrice
 
+  const dispatch = useDispatch()
+
   if (!cart.length) return <EmptyCart />
 
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
       {/* <Form method='POST' action="/order/new"> */}
+
+      <button onClick={() => dispatch(fetchAddress())}>Fetch Address</button>
 
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
